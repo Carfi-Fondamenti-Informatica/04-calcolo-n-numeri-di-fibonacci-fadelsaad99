ae41b7851e2d7bb81bd9c11899013c2c5ea40783
fadelsaad99 <118170848+fadelsaad99@users.noreply.github.com>
2022-11-23 21:03:58 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
-using namespace std;
+  using namespace std;
 
-int main() {
-   return 0;
-}
+  int main() {
+    int n=0, x=1, y=1, z=0;
+    cin >> n;
+    if (n>=2) {
+        cout << x << endl << y << endl;
+        for (int i = 2; i <= n; i++) {
+            z=x+y;
+            x=y;
+            y=z;
+            cout << z << endl;
+        }}
+    else (
+        cout << "errore" );
+
+        return 0;
+
+    }
